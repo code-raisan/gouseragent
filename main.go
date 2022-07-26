9728297485bb4c9d4d37589644bd15ba35003c82
code-raisan <ytpc26ms@outlook.jp>
2022-07-27 01:47:00 +0900

    first

--- main.go
+++ main.go
@@ -1,3 +1,42 @@
 package gouseragent
 
+// Mozilla Firefox
 var MozillaFirefoxWindows string = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0"
+
+var MozillaFirefoxLinux string = "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/100.0"
+
+var MozillaFirefoxMac string = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12.0; rv:10.0) Gecko/20100101 Firefox/100.0"
+
+var MozillaFirefoxAndroid string = "Mozilla/5.0 (Android 11; Mobile; rv:100.0) Gecko/100.0 Firefox/100.0"
+
+// Google Chrome
+var GoogleChromeWindows string = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/605.1.15"
+
+var GoogleChromeMac string = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/605.1.15"
+
+var GoogleChromeAndroid string = "Mozilla/5.0 (Linux; Android 11; Pixel 4) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/100.0.4896.60 Mobile Safari/605.1.15"
+
+var GoogleChromeIos string = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/100.0.4896.60 Mobile/15E148 Safari/605.1.15"
+
+// Apple Safari
+var AppleSafariMac string = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Safari/605.1.15"
+
+var AppleSafariIos string = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/605.1.15"
+
+// Google Bot
+var GoogleBot string = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+
+// Microsoft Bing Bot
+var MicrosoftBingBot string = "Mozilla/5.0 (compatible; bingbot/2.0 +http://www.bing.com/bingbot.htm)"
+
+// Twitter Bot
+var TwitterBot string = "Twitterbot/1.0"
+
+// Facebook External Hit
+var FacebookBot string = "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)"
+
+// Discord Bot
+var DiscordBot string = "Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)"
+
+// Slack Link Expanding
+var SlackBot string = "Slackbot-LinkExpanding 1.0 (+https://api.slack.com/robots)"

4780ffd29cfc497828e3da05e8ea5a44e2b35b3a
code-raisan <ytpc26ms@outlook.jp>
2022-07-27 00:34:54 +0900

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/code-raisan/gouseragent
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,3 @@
+package main
+
+var MozillaFirefoxWindows string = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0"
